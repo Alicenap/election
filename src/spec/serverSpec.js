1afa57c3f3b137d7f9ce42ac89e8d29ee32455d1
Alicenap Tavsanli <alicenap@tavsanli.eu>
2016-12-24 17:04:57 +0100

    example tests added

---
+++ src/spec/serverSpec.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Imports
+var request = require("request");
+
+// Base url
+var baseUrl = "http://localhost:80/"
+
+describe("Server", function() {
+  describe("GET /", function() {
+    it("returns status code 200", function(done) {
+	  request.get(baseUrl, function(error, response, body) {
+        expect(response.statusCode).toEqual(200);
+		done();
+      });
+    });
+    it("returns Hello world", function(done) {
+      request.get(baseUrl, function(error, response, body) {
+		expect(body).toBe("Hello world");
+		done();
+      });
+    });
+  });
+});
